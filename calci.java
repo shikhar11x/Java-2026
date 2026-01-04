bf203fe03d65b30f187c6da170ee59f4f8418e30
Shikhar bajpai <142872564+shikhar-blaziee@users.noreply.github.com>
2026-01-04 19:11:26 +0530

    Added test.java

---
+++ calci.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class calci {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        char oper = sc.next().charAt(0);
+        switch (oper) {
+            case '+': System.out.println(a+b);
+                break;
+            case '-': System.out.println(a-b);
+                break;
+            case '*': System.out.println(a*b);
+                break;
+            case '/': System.out.println(a/b);
+                break;
+            case '%': System.out.println(a%b);
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+
+        int no = 4;
+
+        String type = (no % 2 == 0) ? "Even" : "Odd";
+        System.out.println(type);
+    }
+}
